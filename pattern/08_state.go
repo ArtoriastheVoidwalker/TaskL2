a85e008f6e314372ac4aa73b4196c9c26ab0c8f9
ArtoriastheVoidwalker <mrwinrar.fan@gmail.com>
2023-02-05 14:53:53 +0300

    added pattern state

--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,1 +1,239 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+/*
+ State поведенческий паттерн, который позволяет объектам менять поведение в зависимости от своего состояния.
+ Извне создаётся впечатление, что изменился класс объекта.
+ Применимость:
+ 1) Когда есть объект, поведение которого кардинально меняется в зависимости от внутреннего состояния,
+ причём типов состояний много, и их код часто меняется.
+ 2) Когда код класса содержит множество больших, похожих друг на друга, условных операторов,
+ которые выбирают поведения в зависимости от текущих значений полей класса.
+ 3) Когда используется табличная машина состояний, построенная на условных операторах,
+ но необходимо мириться с дублированием кода для похожих состояний и переходов.
+ Плюсы:
+ 1) Избавляет от множества больших условных операторов машины состояний.
+ 2) Концентрирует в одном месте код, связанный с определённым состоянием.
+ 3) Упрощает код контекста.
+ Минусы:
+ 1) Может неоправданно усложнить код, если состояний мало и они редко меняются.
+*/
+
+// паттерн проектирования State в контексте торговых автоматов.
+
+type VendingMachine struct { // Контекст  хранит ссылку на объект состояния и делегирует ему часть работы,
+	hasItem       State // зависящей от состояний. Контекст работает с этим объектом через общий интерфейс состояний.
+	itemRequested State // Контекст должен иметь метод для присваивания ему нового объекта-состояния.
+	hasMoney      State
+	noItem        State
+
+	currentState State
+
+	itemCount int
+	itemPrice int
+}
+
+func newVendingMachine(itemCount, itemPrice int) *VendingMachine {
+	v := &VendingMachine{
+		itemCount: itemCount,
+		itemPrice: itemPrice,
+	}
+	hasItemState := &HasItemState{
+		vendingMachine: v,
+	}
+	itemRequestedState := &ItemRequestedState{
+		vendingMachine: v,
+	}
+	hasMoneyState := &HasMoneyState{
+		vendingMachine: v,
+	}
+	noItemState := &NoItemState{
+		vendingMachine: v,
+	}
+
+	v.setState(hasItemState)
+	v.hasItem = hasItemState
+	v.itemRequested = itemRequestedState
+	v.hasMoney = hasMoneyState
+	v.noItem = noItemState
+	return v
+}
+
+func (v *VendingMachine) requestItem() error {
+	return v.currentState.requestItem()
+}
+
+func (v *VendingMachine) addItem(count int) error {
+	return v.currentState.addItem(count)
+}
+
+func (v *VendingMachine) insertMoney(money int) error {
+	return v.currentState.insertMoney(money)
+}
+
+func (v *VendingMachine) dispenseItem() error {
+	return v.currentState.dispenseItem()
+}
+
+func (v *VendingMachine) setState(s State) {
+	v.currentState = s
+}
+
+func (v *VendingMachine) incrementItemCount(count int) {
+	fmt.Printf("Adding %d items\n", count)
+	v.itemCount = v.itemCount + count
+}
+
+type State interface { // Интерфейс состояния описывает общий интерфейс для всех конкретных состояний
+	addItem(int) error
+	requestItem() error
+	insertMoney(money int) error
+	dispenseItem() error
+}
+
+type NoItemState struct { // Конкретный интерфейс реализуют поведения, связанные с определённым состоянием контекста.
+	vendingMachine *VendingMachine // Иногда приходится создавать целые иерархии классов состояний, чтобы обобщить дублирующий код.
+	// Состояние может иметь обратную ссылку на объект контекста.
+	// Через неё не только удобно получать из контекста нужную информацию, но и осуществлять смену его состояния.
+}
+
+func (i *NoItemState) requestItem() error {
+	return fmt.Errorf("Item out of stock")
+}
+
+func (i *NoItemState) addItem(count int) error {
+	i.vendingMachine.incrementItemCount(count)
+	i.vendingMachine.setState(i.vendingMachine.hasItem)
+	return nil
+}
+
+func (i *NoItemState) insertMoney(money int) error {
+	return fmt.Errorf("Item out of stock")
+}
+func (i *NoItemState) dispenseItem() error {
+	return fmt.Errorf("Item out of stock")
+}
+
+type HasItemState struct { // Конкретный интерфейс
+	vendingMachine *VendingMachine
+}
+
+func (i *HasItemState) requestItem() error {
+	if i.vendingMachine.itemCount == 0 {
+		i.vendingMachine.setState(i.vendingMachine.noItem)
+		return fmt.Errorf("No item present")
+	}
+	fmt.Printf("Item requestd\n")
+	i.vendingMachine.setState(i.vendingMachine.itemRequested)
+	return nil
+}
+
+func (i *HasItemState) addItem(count int) error {
+	fmt.Printf("%d items added\n", count)
+	i.vendingMachine.incrementItemCount(count)
+	return nil
+}
+
+func (i *HasItemState) insertMoney(money int) error {
+	return fmt.Errorf("Please select item first")
+}
+func (i *HasItemState) dispenseItem() error {
+	return fmt.Errorf("Please select item first")
+}
+
+type ItemRequestedState struct { // Конкретный интерфейс
+	vendingMachine *VendingMachine
+}
+
+func (i *ItemRequestedState) requestItem() error {
+	return fmt.Errorf("Item already requested")
+}
+
+func (i *ItemRequestedState) addItem(count int) error {
+	return fmt.Errorf("Item Dispense in progress")
+}
+
+func (i *ItemRequestedState) insertMoney(money int) error {
+	if money < i.vendingMachine.itemPrice {
+		return fmt.Errorf("Inserted money is less. Please insert %d", i.vendingMachine.itemPrice)
+	}
+	fmt.Println("Money entered is ok")
+	i.vendingMachine.setState(i.vendingMachine.hasMoney)
+	return nil
+}
+func (i *ItemRequestedState) dispenseItem() error {
+	return fmt.Errorf("Please insert money first")
+}
+
+type HasMoneyState struct { // Конкретный интерфейс
+	vendingMachine *VendingMachine
+}
+
+func (i *HasMoneyState) requestItem() error {
+	return fmt.Errorf("Item dispense in progress")
+}
+
+func (i *HasMoneyState) addItem(count int) error {
+	return fmt.Errorf("Item dispense in progress")
+}
+
+func (i *HasMoneyState) insertMoney(money int) error {
+	return fmt.Errorf("Item out of stock")
+}
+func (i *HasMoneyState) dispenseItem() error {
+	fmt.Println("Dispensing Item")
+	i.vendingMachine.itemCount = i.vendingMachine.itemCount - 1
+	if i.vendingMachine.itemCount == 0 {
+		i.vendingMachine.setState(i.vendingMachine.noItem)
+	} else {
+		i.vendingMachine.setState(i.vendingMachine.hasItem)
+	}
+	return nil
+}
+
+func main() {
+	vendingMachine := newVendingMachine(1, 10)
+
+	err := vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.addItem(2)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
