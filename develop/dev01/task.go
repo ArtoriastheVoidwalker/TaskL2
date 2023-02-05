cc0bfe06315f65f8b0d0e2768fd852448471f020
ArtoriastheVoidwalker <mrwinrar.fan@gmail.com>
2023-02-05 18:44:05 +0300

    added task dev01

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,1 +1,16 @@
-package dev01
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Current time: %s\n", time.Format("15:04:05"))
+}
